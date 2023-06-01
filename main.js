aca6279f14786bbfdf732969b1cc75a6a71b42d8
Emre Ilhan <emrilhan@gmail.com>
2023-06-01 15:52:51 +0300

    basic functions half-complete

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 let firstNumber;
 let secondNumber;
 let operator;
+let displayValue;
 
 const display = document.getElementById('display');
 const btn = document.querySelectorAll('.cell');
-
 btn.forEach((i) => i.addEventListener('click', show));
 
 function show() {
-  console.log('clicked');
   display.innerHTML = this.id;
 }
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+  display.textContent = '0';
+});
+
+const deleteBtn = document.querySelector('#delete');
+deleteBtn.addEventListener('click', () => {
+  display.textContent = '0';
+});
+
+const equalBtn = document.querySelector('.equal');
 
 function operate(firstNumber, secondNumber, operator) {
   switch (operator) {
