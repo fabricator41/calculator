c366802c985559841b5069d9011487a64a70057f
Emre Ilhan <emrilhan@gmail.com>
2023-06-02 14:26:05 +0300

    second display added

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
-let firstNumber;
-let secondNumber;
-let operator;
+/* User input initialized */
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
 let displayValue;
 
 const display = document.getElementById('display');
 const btn = document.querySelectorAll('.cell');
 btn.forEach((i) => i.addEventListener('click', show));
 
+const display2 = document.getElementById('display2');
+
 function show() {
-  display.textContent = `${this.id}`;
+  display.textContent = display.textContent + `${this.textContent}`;
+  firstNumber = display.innerHTML;
+}
+
+const operatorBtn = document.querySelectorAll('.operator');
+operatorBtn.forEach((i) => i.addEventListener('click', operatorChoice));
+
+function operatorChoice() {
+  operator = this.id;
 }
 
 const clearBtn = document.querySelector('#clear');
 clearBtn.addEventListener('click', () => {
-  display.textContent = '0';
+  display.textContent = '';
 });
 
 const deleteBtn = document.querySelector('#delete');
 deleteBtn.addEventListener('click', () => {
-  display.textContent = '0';
+  display.textContent = '';
 });
 
 const equalBtn = document.querySelector('.equal');
 
-function operate(firstNumber, secondNumber, operator) {
+function operate(fNumber, sNumber, ope) {
+  firstNumber = fNumber;
+  secondNumber = sNumber;
+  operator = ope;
+  let result = 0;
   switch (operator) {
     case 'add':
-      add(firstNumber, secondNumber);
+      result = add(firstNumber, secondNumber);
       break;
     case 'subtract':
-      subtract(firstNumber, secondNumber);
+      result = subtract(firstNumber, secondNumber);
       break;
     case 'multiply':
-      multiply(firstNumber, secondNumber);
+      result = multiply(firstNumber, secondNumber);
       break;
     case 'divide':
-      divide(firstNumber, secondNumber);
+      result = divide(firstNumber, secondNumber);
       break;
   }
+  display2.textContent = result;
 }
+
+equalBtn.addEventListener('click', operate);
 
 function add(a, b) {
   return a + b;
